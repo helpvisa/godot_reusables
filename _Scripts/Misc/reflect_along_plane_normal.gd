6a207abcf87986d6bfe1981e764f04f7a6e14f1f
Daniel Brackenbury <daniel.brackenbury@gmail.com>
2024-04-22 21:05:35 -0400

    fixed code snippet

--- _Scripts/Misc/reflect_along_plane_normal.gd
+++ _Scripts/Misc/reflect_along_plane_normal.gd
@@ -3,5 +3,5 @@ extends RefCounted
 # simple code snippet for mirroring a vector along a given plane
 # put it into a class of its own, make it global, etc.
 func mirror_along_normal(vector, normal):
-	return -vector + 2 * -normal * vector.dot(normal)
+	return -vector + 2 * normal * vector.dot(normal)
 
